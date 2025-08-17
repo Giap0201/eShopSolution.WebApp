e982b119831e8584d269354f3a49ffc86563b473
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-08-17 23:55:49 +0700

    create_configuration

---
+++ eShopSolution.Data/Entities/Promotion.cs
@@ -0,0 +1,25 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool ApplyForAll { get; set; }
+        public int ? DiscountPrecent { get; set; }  
+        public decimal? DiscountAmount { get; set; }
+        public string ProductIds { get; set; }  
+        public string CategoryIds { get; set; } 
+        public string Name { get; set; }
+        public Status Status { get; set; }
+
+
+    }
+}
