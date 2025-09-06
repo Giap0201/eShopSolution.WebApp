a358e474ecf52f1be93a637632816857bff01737
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-09-06 21:39:56 +0700

    create-swagger

--- eShopSolution.BackendApi/Program.cs
+++ eShopSolution.BackendApi/Program.cs
@@ -1,4 +1,4 @@
-using eShopSolution.Application.Catalog.Products;
+﻿using eShopSolution.Application.Catalog.Products;
 using eShopSolution.Data.EF;
 using EShopSulotionUtilities.Constants;
 using Microsoft.EntityFrameworkCore;
@@ -8,22 +8,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-    
 // Configure DbContext with SQL Server
-
 builder.Services.AddDbContext<EShopDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString(SystemConstant.MainConnectionString)));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString(SystemConstant.MainConnectionString)));
 
 // Add other services (e.g., repositories, application services) here
 builder.Services.AddTransient<IPublicProductService, PublicProductService>();
 
+// 🔹 Thêm Swagger service để đăng ký ISwaggerProvider
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    // Thêm Swagger middleware vào pipeline
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "eShopSolution API V1");
+        // c.RoutePrefix = string.Empty; // mở Swagger ở trang gốc
+    });
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
