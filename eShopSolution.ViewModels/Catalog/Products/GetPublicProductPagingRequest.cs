66fe1276c6570de4c0f6429761c0f38caaf0a3c1
Nguyễn Hữu Giáp <giap13022005@@gmail.com>
2025-09-06 09:27:04 +0700

    add_image

---
+++ eShopSolution.ViewModels/Catalog/Products/GetPublicProductPagingRequest.cs
@@ -0,0 +1,9 @@
+﻿using eShopSolution.ViewModels.Common;
+
+namespace eShopSolution.ViewModels.Catalog.Products
+{
+    public class GetProductPagingRequest : PagingRequestBase
+    {
+        public int? CategoryId { get; set; }
+    }
+}
